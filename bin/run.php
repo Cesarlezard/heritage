b091f5c62358fecae77b37b0f9722fd021fa3ae6
William <work@suppo.fr>
2021-12-05 22:38:29 +0100

    Les méthodes d'un Simpson.

--- bin/run.php
+++ bin/run.php
@@ -2,30 +2,13 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$bart = new \App\Simpson();
-$bart->name = 'Bart';
-$bart->dateOfBirth = '1980-02-23';
-$bart->gender = 'male';
+$simpsons = [
+    new \App\Simpson('Bart', '1980-02-23', 'male'),
+    new \App\Simpson('Lisa', '1982-05-09', 'female'),
+    new \App\Simpson('Marge', '1956-04-01', 'female'),
+    new \App\Simpson('Homer', '1956-05-12', 'male'),
+];
 
-echo $bart->name . ' | ' . $bart->dateOfBirth . ' | ' . $bart->gender . "\n";
-
-$lisa = new \App\Simpson();
-$lisa->name = 'Lisa';
-$lisa->dateOfBirth = '1982-05-09';
-$lisa->gender = 'female';
-
-echo $lisa->name . ' | ' . $lisa->dateOfBirth . ' | ' . $lisa->gender . "\n";
-
-$marge = new \App\Simpson();
-$marge->name = 'Marge';
-$marge->dateOfBirth = '1956-01-01';
-$marge->gender = 'female';
-
-echo $marge->name . ' | ' . $marge->dateOfBirth . ' | ' . $marge->gender . "\n";
-
-$homer = new \App\Simpson();
-$homer->name = 'Homer';
-$homer->dateOfBirth = '1956-05-12';
-$homer->gender = 'male';
-
-echo $homer->name . ' | ' . $homer->dateOfBirth . ' | ' . $homer->gender . "\n";
+foreach ($simpsons as $simpson) {
+    echo $simpson->getName() . " | age: " . $simpson->getAge() . "\n";
+}
--- src/Simpson.php
+++ src/Simpson.php
@@ -2,9 +2,27 @@
 
 namespace App;
 
+use DateTime;
+
 class Simpson
 {
-    public $name;
-    public $dateOfBirth;
-    public $gender;
+    public function __construct(
+        public $name,
+        public $dateOfBirth,
+        public $gender
+    ) {}
+
+    public function getName()
+    {
+        return $this->name . ' Simpson';
+    }
+
+    public function getAge()
+    {
+        $dateOfBirth = (new Datetime($this->dateOfBirth));
+
+        $now = new Datetime();
+
+        return $dateOfBirth->diff($now)->y;
+    }
 }
