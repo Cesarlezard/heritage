1beca88b08a33e2d09cd8ebd4a615130ac2c6a07
William <work@suppo.fr>
2021-12-06 16:52:08 +0100

    Implémentation exercice 2.

--- bin/run.php
+++ bin/run.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $simpsons = [
-    new \App\Simpson('Bart', '1980-02-23', 'male'),
+    $bart = new \App\Simpson('Bart', '1980-02-23', 'male'),
     new \App\Simpson('Lisa', '1982-05-09', 'female'),
     new \App\Simpson('Marge', '1956-04-01', 'female'),
     new \App\Simpson('Homer', '1956-05-12', 'male'),
@@ -11,4 +11,12 @@ $simpsons = [
 
 foreach ($simpsons as $simpson) {
     echo $simpson->getName() . " | age: " . $simpson->getAge() . "\n";
+}
+
+echo "-------------\n";
+
+$bart->heal('beer');
+
+for($i = 0; $i < 10; $i++) {
+    echo $bart->hp . ' | ' . $bart->run(50) . ' | ' . $bart->hp .  "\n";
 }
--- src/Simpson.php
+++ src/Simpson.php
@@ -9,7 +9,8 @@ class Simpson
     public function __construct(
         public $name,
         public $dateOfBirth,
-        public $gender
+        public $gender,
+        public $hp = 50
     ) {}
 
     public function getName()
@@ -24,5 +25,43 @@ class Simpson
         $now = new Datetime();
 
         return $dateOfBirth->diff($now)->y;
+    }
+
+    public function olderThan($simpson)
+    {
+        return $this->getAge() > $simpson->getAge();
+    }
+
+    public function heal($food)
+    {
+        switch ($food) {
+            case 'donuts':
+                $this->hp = 100;
+                break;
+            case 'beer':
+                $this->hp += 5;
+                break;
+            case 'beans':
+                $this->hp  += 20;
+                break;
+        }
+
+        if ($this->hp > 100) {
+            $this->hp = 100;
+        }
+    }
+
+    public function run($time)
+    {
+        $speed = 100 * $this->hp;
+        $distance = $speed * $time;
+
+        $this->hp -=10;
+
+        if ($this->hp < 0) {
+            $this->hp = 0;
+        }
+
+        return $distance;
     }
 }
